94764ac449403291849988edfe86e588f9350105
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2019-03-02 23:28:24 +0000

    Run second test against Auth API

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,8 +1,11 @@
 import io.restassured.RestAssured;
 import io.restassured.config.RestAssuredConfig;
+import io.restassured.response.Response;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ApiTest {
@@ -10,6 +13,14 @@ public class ApiTest {
     @Test
     public void myFirstRaTest(){
         assertThat(RestAssured.config(), instanceOf(RestAssuredConfig.class));
+    }
+
+    @Test
+    public void mySecondRaTest(){
+        Response response = given().get("https://automationintesting.online/auth/actuator/health");
+        int statusCode = response.getStatusCode();
+
+        assertThat(statusCode, is(200));
     }
 
 }
